2bdc990a6b888b9979735bfad0f32545b8bd7191
lakhalouahid <lakhalouahid@gmail.com>
2022-01-31 10:05:00 +0100

    first commit

---
+++ app/Http/Controllers/PredictController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PredictController extends Controller
+{
+  public function predict(Request $request)
+  {
+    $dataset = $request->dataset;
+    $x1 = $request->x1;
+    $x2 = $request->x2;
+    $command = 'bash -c "cd scripts/zombie && python zombie.py -d ' . $dataset . ' --x1 ' . $x1 . ' --x2 ' . $x2 . '"';
+
+    $output = shell_exec($command);
+    $prob = 0;
+    if ($output>=0.5){
+      $result = 1;
+      $prob = $output;
+    } else {
+      $result = 0;
+      $prob = 1 - $output;
+    }
+    return response()->json(['result' => $result, 'prob' => $prob]);
+  }
+
+}
